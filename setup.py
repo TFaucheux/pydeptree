e2c60840f82dcc8f20e35f5f365d9c8cea338b35
TFaucheux <tfaucheux@gmail.com>
2025-07-23 14:30:45 -0400

    Release v0.3.17
    Bump version to 0.3.17 and update changelog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pydeptree",
-    version="0.3.16",
+    version="0.3.17",
     author="Todd Faucheux",
     author_email="tfaucheux@gmail.com",
     description="A Python dependency tree analyzer with rich output",
