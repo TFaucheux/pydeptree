5d7fa5896d590662ad0c618c8065da40497acf02
TFaucheux <tfaucheux@gmail.com>
2025-07-23 11:59:30 -0400

    Release v0.3.11
    Bump version to 0.3.11 and update changelog

--- pydeptree/__init__.py
+++ pydeptree/__init__.py
@@ -1,2 +1,2 @@
 """Python Dependency Tree Analyzer"""
-__version__ = "0.3.10"
+__version__ = "0.3.11"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pydeptree",
-    version="0.3.10",
+    version="0.3.11",
     author="Todd Faucheux",
     author_email="tfaucheux@gmail.com",
     description="A Python dependency tree analyzer with rich output",
